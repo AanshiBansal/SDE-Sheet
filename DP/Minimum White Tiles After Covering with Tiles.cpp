09c0e92820e57840ee7a0070fb08e7644970f8e9
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-03-20 13:54:55 +0530

    DP

--- DP/Minimum White Tiles After Covering with Tiles.cpp
+++ DP/Minimum White Tiles After Covering with Tiles.cpp
@@ -38,3 +38,63 @@ public:
         return rec(0,0);
     }
 };
+
+// Tabulatiom
+// Bottom Up DP
+// Time: O(numCarpets*floor.size())
+// Space: O(numCarpets*floor.size())
+class Solution {
+public:
+    int minimumWhiteTiles(string floor, int numCarpets, int carpetLen) {
+        int n=floor.size();
+        vector<vector<int>>dp(numCarpets+1,vector<int>(n+1,0));
+        int curr=0;
+        for(int i=0;i<n;i++){
+            if(floor[i]=='1')
+                curr++;
+            dp[0][i+1]=curr;
+        }
+        for(int i=1;i<=numCarpets;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j]=dp[i][j-1]+floor[j-1]-'0';
+                if(j-carpetLen>=0){
+                    dp[i][j]=min(dp[i][j],dp[i-1][j-carpetLen]);
+                }else
+                    dp[i][j]=0;
+            }
+        }
+        return dp[numCarpets][n];
+    }
+};
+
+// Space Optmisation
+// Bottom Up DP
+// Time: O(numCarpets*floor.size())
+// Space: O(floor.size())
+class Solution {
+public:
+    int minimumWhiteTiles(string floor, int numCarpets, int carpetLen) {
+        int n=floor.size();
+        vector<int>dp(n+1);
+        dp[0]=0;
+        vector<int>temp(n+1);
+        int curr=0;
+        for(int i=0;i<n;i++){
+            if(floor[i]=='1')
+                curr++;
+            dp[i+1]=curr;
+        }
+        for(int i=1;i<=numCarpets;i++){
+            temp[0]=0;
+            for(int j=1;j<=n;j++){
+                temp[j]=temp[j-1]+floor[j-1]-'0';
+                if(j-carpetLen>=0){
+                    temp[j]=min(temp[j],dp[j-carpetLen]);
+                }else
+                    temp[j]=0;
+            }
+            dp=temp;
+        }
+        return dp[n];
+    }
+};
