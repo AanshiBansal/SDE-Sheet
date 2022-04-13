59253210e4690189c413764661b251e5e053e3fe
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-04-13 21:41:53 +0530

    3 Sum versions

---
+++ Array/3Sum Closest.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/3sum-closest/
+//2-Pointer Approach
+//Time: O(n^2)
+//Space: O(n) Auxiliary Array because of sorting
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int ans=nums[0]+nums[1]+nums[2];
+        for(int i=0;i<=n-3;i++){
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int curr=nums[i]+nums[j]+nums[k];
+                if(abs(target-curr)<abs(target-ans))
+                    ans=curr;
+                if(curr==target)
+                    return target;
+                if(curr<target)
+                    j++;
+                else
+                    k--;
+            }
+        }
+        return ans;
+    }
+};
